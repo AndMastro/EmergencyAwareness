1dbe46615ea9e62616e857a2993eb7a6b5c1cdf4
AndMastro <andrea.mastropietro@gmail.com>
2018-07-07 17:54:23 +0200

    Edited ClassificaitionMain

--- src/ClassificationMain.py
+++ src/ClassificationMain.py
@@ -1,24 +1,12 @@
 
-# coding: utf-8
-
-# In[5]:
-
-
 from ClassifierCreator import *
 from DatasetGetter import *
-
-
-# In[ ]:
-
 
 #choose class between Relevant, Information, Action, Report, Sentiment, Movement, Preparation
 [xTrain, xTest, yTrain, yTest] = datasetGetter("Information")
 target_names = ["Non Information", "Information"]
 
-
-# In[ ]:
-
-
+#choose baseline classifier of all feature classifier w/ or w/out feature selection
 cl = pipelineClassifier()
 #cl = baselineClassifier()
 scores = kFoldCrossVal(cl, xTrain, yTrain, 5)
